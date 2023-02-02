2701ab7bda106bf22e862c128fb5891a370f287e
300354149 <regalroyacruz@gmail.com>
2023-02-02 13:10:53 -0800

    comment5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is roy comment
+        //this is roy comment 3
         //Jess comment 4
     }
 }
