cb71bfc8eb81e357104066b75aa17bb9a54e7841
300354149 <regalroyacruz@gmail.com>
2023-02-02 13:00:09 -0800

    comment3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is roy comment
-        //this is roy comment 2
+        //this is roy comment 3
     }
 }
